fb02053056b2f0975b1c215f07330fbdb5206010
HeosFx <82534081+HeosFx@users.noreply.github.com>
2023-06-13 09:51:32 +0200

    Add more info page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ProductPageComponent } from './components/product-page/product-page.com
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { MoreInfoComponent } from './components/more-info/more-info.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: 'market', component: ProductListComponent},
   {path: 'product/:id', component: ProductPageComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'more-info', component: MoreInfoComponent}
 ]
 
 @NgModule({
