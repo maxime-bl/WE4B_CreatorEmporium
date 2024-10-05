65e0ef013a1fb780b5dfd9617c129d4b725bc33d
Maxime <57617345+maxime-bl@users.noreply.github.com>
2024-10-05 10:43:48 -0400

    Removed unnecessary files and updated .gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   baseUrl: 'http://localhost:4200',
   firebase: {
-    projectId: 'we4bcreatoremporium',
-    appId: '1:542645259589:web:1dbb3a44fab617f265422c',
-    storageBucket: 'we4bcreatoremporium.appspot.com',
-    apiKey: 'AIzaSyDl0K8BmhQ2juQu-Z6xp2CcMLLdVj26Qlk',
-    authDomain: 'we4bcreatoremporium.firebaseapp.com',
-    messagingSenderId: '542645259589',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
 };
